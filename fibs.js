a2111c3bce52a75193313e945263178daaf4b8bb
Tomas Lobato <tomaslobatodev@gmail.com>
2023-09-09 13:43:44 -0300

    recursive fibs corrected

--- fibs.js
+++ fibs.js
@@ -11,16 +11,23 @@ function fibs(n) {
   return result
 }
 
-console.log("fibs (not recursive) input = 6", fibs(6))
+console.log("fibs (not recursive) input = 6", fibs(10))
 
 //recursive
-function fibsRecursive(n, array = [0, 1]) {
+function fibsRecursive(n, array = []) {
   if (n <= 0) {
     return array;
   }
 
-  array.push(array[array.length - 1] + array[array.length - 2]);
-  return fibsRecursive(n - 1.5 , array);
+  if (array.length === 0) {
+    array.push(0);
+  } else if (array.length === 1) {
+    array.push(1);
+  } else {
+    array.push(array[array.length - 1] + array[array.length - 2]);
+  }
+
+  return fibsRecursive(n - 1, array);
 }
 
-console.log("fibs (recursive) input = 8", fibsRecursive(8));
+console.log("fibs (recursive) input = 10", fibsRecursive(12));
